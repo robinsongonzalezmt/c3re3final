cca5583aa1ffca0bea3b728fe914900fc67779b4
robinsongonzalezmt <robinson.gonzalez.mt@usa.edu.co>
2022-10-12 18:59:13 -0500

    VfinalReto3 12-10-22

---
+++ demo/src/main/java/co/edu/usa/controlador/ReservationControlador.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.usa.controlador;
+
+import co.edu.usa.modelo.Reservation;
+import co.edu.usa.servicio.ReservationServicio;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author cala0
+ */
+@RestController
+@RequestMapping ("/api/Reservation")
+@CrossOrigin(origins = "*")  ///conexion front con cualquier IP de ORACLE
+
+public class ReservationControlador {
+      @Autowired
+    private ReservationServicio reservationServicio;
+    
+//    @CrossOrigin(origins = "http://132.226.240.254")
+    @GetMapping("/all")
+      public List<Reservation> getReservations() {
+          return reservationServicio.getAll();
+      }
+
+//    @CrossOrigin(origins = "http://132.226.240.254")
+    @GetMapping("/{id}")
+      public Optional<Reservation> getreservation(@PathVariable("id") int reservationId) {
+          return reservationServicio.getReservation(reservationId);
+      }
+
+//    @CrossOrigin(origins = "http://132.226.240.254")
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED ) 
+      public Reservation save(@RequestBody Reservation reservation) {
+          return reservationServicio.save(reservation);
+      } 
+      
+      
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED ) 
+    public Reservation update(@RequestBody Reservation reservation) {
+          return reservationServicio.update(reservation);};
+
+    @DeleteMapping("/{id}")
+      public void delete(@PathVariable("id") int reservationId) {reservationServicio.deleteReservation(reservationId);};
+     
+}
---
+++ demo/src/main/java/co/edu/usa/servicio/ReservationServicio.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.usa.servicio;
+
+import co.edu.usa.modelo.Reservation;
+import co.edu.usa.repositorio.ReservationRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author cala0
+ */
+
+@Service
+public class ReservationServicio {
+     @Autowired
+    private ReservationRepository reservationRepository;
+    
+   public List<Reservation> getAll() {
+   return (List<Reservation>) reservationRepository.getAll();};
+  
+  public Optional<Reservation> getReservation(int id) {return reservationRepository.getReservation(id);};
+  
+  public Reservation save(Reservation reservation) { 
+       if (reservation.getIdReservation() == null){
+           return reservationRepository.save(reservation);
+       }
+       else
+       {
+          Optional<Reservation> co =  reservationRepository.getReservation(reservation.getIdReservation());
+          if (co.isEmpty()){
+              return reservationRepository.save(reservation);
+          }
+          else
+          {
+              return reservation;
+          }
+       }
+    }
+  public boolean deleteReservation(int reservationId){
+        Boolean d=getReservation(reservationId).map(reservation -> {
+            reservationRepository.delete(reservation);
+            return true;
+        }).orElse(false);
+        return d;
+    }
+
+public Reservation update(Reservation reservation){
+        if(reservation.getIdReservation()!=null){
+            Optional<Reservation> e= reservationRepository.getReservation(reservation.getIdReservation());
+            if(!e.isEmpty()){
+
+                if(reservation.getStartDate()!=null){
+                    e.get().setStartDate(reservation.getStartDate());
+                }
+                if(reservation.getDevolutionDate()!=null){
+                    e.get().setDevolutionDate(reservation.getDevolutionDate());
+                }
+                if(reservation.getStatus()!=null){
+                    e.get().setStatus(reservation.getStatus());
+                }
+                reservationRepository.save(e.get());
+                return e.get();
+            }else{
+                return reservation;
+            }
+        }else{
+            return reservation;
+        }
+    }
+}
